5d763fde3d4aabc3c194ed9ad37bf31471854ce0
Mikhail Shulga <mmshulga@gmail.com>
2018-12-08 23:50:33 +0300

    Added bootstrapping, fixed some typos.

--- src/main/java/my/mmshulga/springrestmvc/model/Category.java
+++ src/main/java/my/mmshulga/springrestmvc/model/Category.java
@@ -1,12 +1,14 @@
 package my.mmshulga.springrestmvc.model;
 
 import lombok.Data;
-import org.springframework.data.annotation.Id;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Data
+@Entity
 public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/main/java/my/mmshulga/springrestmvc/repositories/CategoryRepository.java
+++ src/main/java/my/mmshulga/springrestmvc/repositories/CategoryRepository.java
@@ -2,9 +2,5 @@ package my.mmshulga.springrestmvc.repositories;
 
 import my.mmshulga.springrestmvc.model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Component;
 
-@Component
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-
-}
+public interface CategoryRepository extends JpaRepository<Category, Long> { }
