e5f7512a318404ba22f5d7c7ab4257f91d0563ee
Mikhail Shulga <mmshulga@gmail.com>
2018-12-09 00:21:51 +0300

    Introduced mapstruct.

---
+++ src/main/java/my/mmshulga/api/v1/mapper/CategoryMapper.java
@@ -0,0 +1,13 @@
+package my.mmshulga.api.v1.mapper;
+
+import my.mmshulga.api.v1.model.CategoryDTO;
+import my.mmshulga.springrestmvc.model.Category;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface CategoryMapper {
+    CategoryMapper INSTANCE = Mappers.getMapper(CategoryMapper.class);
+
+    CategoryDTO categoryToCategoryDTO(Category category);
+}
