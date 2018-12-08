1b6f9bf4b4829c80e1b02e5d57ac233e32a66064
Mikhail Shulga <mmshulga@gmail.com>
2018-12-08 23:38:01 +0300

    Initial empty project with the dependencies.

---
+++ src/test/java/my/mmshulga/springrestmvc/SpringRestMvcApplicationTests.java
@@ -0,0 +1,16 @@
+package my.mmshulga.springrestmvc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringRestMvcApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
